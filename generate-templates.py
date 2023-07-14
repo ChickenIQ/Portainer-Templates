d1e1544f6ac78b2dec1f427910b8541e6f17e622
ChickenIQ <emibenzar@gmail.com>
2023-07-15 01:48:35 +0300

    template generation script

---
+++ generate-templates.py
@@ -0,0 +1,17 @@
+import json
+import os
+
+base = {
+    "version": "2",
+    "templates": []
+}
+
+
+for root, dirs, files in os.walk("templates"):
+    for file in files:
+        if file.endswith(".json"):
+            with open(os.path.join(root, file), "r") as template:
+                base["templates"].append(json.load(template))
+
+with open('templates.json', 'w') as outfile:
+    json.dump(base, outfile, indent=4)
